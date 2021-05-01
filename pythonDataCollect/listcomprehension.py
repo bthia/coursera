920e9de448b33906e422a35a2044fb1800091980
Boon Sing Thia <thia@google.com>
2021-05-01 14:22:49 +0000

    update listcomrehension.py with excercise

--- pythonDataCollect/listcomprehension.py
+++ pythonDataCollect/listcomprehension.py
@@ -16,3 +16,13 @@ things = [2,4,5,6,12,24,245, 234]
 print(list(map(lambda value: value * 2, filter(lambda invalue: invalue % 2 == 0, things))))
 
 print(list(v * 2 for v in things if v%2 == 0))
+
+
+print('\nExample 4: excercise *************')
+tester = {'info': [{"name": "Lauren", 'class standing': 'Junior', 'major': "Information Science"},{'name': 'Ayo', 'class standing': "Bachelor's", 'major': 'Information Science'}, {'name': 'Kathryn', 'class standing': 'Senior', 'major': 'Sociology'}, {'name': 'Nick', 'class standing': 'Junior', 'major': 'Computer Science'}, {'name': 'Gladys', 'class standing': 'Sophomore', 'major': 'History'}, {'name': 'Adam', 'major': 'Violin Performance', 'class standing': 'Senior'}]}
+
+import json
+inner_string = tester['info']
+compri = [n['name'] for n in inner_string]
+print(json.dumps(inner_string, indent=4))
+print(compri)
